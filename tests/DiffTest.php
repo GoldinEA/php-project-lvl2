36f7ee8a32d0e09997caed39a286664fb1259a79
Evgeny Goldin <goldin5@yandex.ru>
2021-09-07 13:29:56 +0400

    add fixes test

--- tests/DiffTest.php
+++ tests/DiffTest.php
@@ -82,211 +82,199 @@ class DiffTest extends TestCase
     ];
 
 
-//    public function testCreateTree()
-//    {
-//        $expected = [
-//            ['name' => 'host', 'type' => 'no_change', 'value' => 'hexlet.io'],
-//            ['name' => 'timeout', 'type' => 'changed', 'value_added' => 20, 'value_deleted' => 50],
-//            ['name' => 'proxy', 'type' => 'deleted', 'value' => '123.234.53.22'],
-//            ['name' => 'follow', 'type' => 'deleted', 'value' => false],
-//            ['name' => 'verbose', 'type' => 'added', 'value' => true],
-//        ];
-//
-//        $tree = createTree($this->dataFirstFile, $this->dataLastFile);
-//        $this->assertEquals($expected, $tree);
-//    }
+    public function testCreateTree()
+    {
+        $expected = [
+            ['name' => 'host', 'type' => 'no_change', 'value' => 'hexlet.io'],
+            ['name' => 'timeout', 'type' => 'changed', 'value_added' => 20, 'value_deleted' => 50],
+            ['name' => 'proxy', 'type' => 'deleted', 'value' => '123.234.53.22', 'multilevel' => false],
+            ['name' => 'follow', 'type' => 'deleted', 'value' => false, 'multilevel' => false],
+            ['name' => 'verbose', 'type' => 'added', 'value' => true, 'multilevel' => false],
+        ];
+
+        $tree = createTree($this->dataFirstFile, $this->dataLastFile);
+        $this->assertEquals($expected, $tree);
+    }
 
     public function testCreateTreeMultilevel()
     {
         $expected = [
-            [
-                'name' => 'common',
-                'type' => 'changed',
-                'multilevel' => true,
-                'value' =>
-                    [
+                [
+                    'name' => 'common',
+                    'type' => 'changed',
+                    'multilevel' => true,
+                    'value' =>
                         [
-                            'name' => 'setting1',
-                            'type' => 'deleted',
-                            'value' => 'Value 1',
-                        ],
-                        [
-                            'name' => 'setting2',
-                            'type' => 'deleted',
-                            'value' => 200,
-                        ],
-                        [
-                            'name' => 'setting3',
-                            'type' => 'deleted',
-                            'value' => true,
-                        ],
-                        [
-                            'name' => 'setting6',
-                            'type' => 'deleted',
-                            'value' =>
                                 [
-                                    'key' => 'value',
-                                    'doge' =>
+                                    'name' => 'setting1',
+                                    'type' => 'no_change',
+                                    'value' => 'Value 1',
+                                ],
+                                [
+                                    'name' => 'setting2',
+                                    'type' => 'deleted',
+                                    'value' => 200,
+                                    'multilevel' => false,
+                                ],
+                                [
+                                    'name' => 'setting3',
+                                    'type' => 'changed',
+                                    'value_added' => NULL,
+                                    'value_deleted' => true,
+                                ],
+                                [
+                                    'name' => 'setting6',
+                                    'type' => 'changed',
+                                    'multilevel' => true,
+                                    'value' =>
                                         [
-                                            'wow' => '',
+                                                [
+                                                    'name' => 'key',
+                                                    'type' => 'no_change',
+                                                    'value' => 'value',
+                                                ],
+                                                [
+                                                    'name' => 'doge',
+                                                    'type' => 'changed',
+                                                    'multilevel' => true,
+                                                    'value' =>
+                                                        [
+                                                                [
+                                                                    'name' => 'wow',
+                                                                    'type' => 'changed',
+                                                                    'value_added' => 'so much',
+                                                                    'value_deleted' => '',
+                                                                ],
+                                                        ],
+                                                ],
+                                                [
+                                                    'name' => 'ops',
+                                                    'type' => 'added',
+                                                    'value' => 'vops',
+                                                    'multilevel' => false,
+                                                ],
                                         ],
                                 ],
-                        ],
-                        [
-                            'name' => 'common',
-                            'type' => 'added',
-                            'value' =>
                                 [
-                                    'follow' => false,
-                                    'setting1' => 'Value 1',
-                                    'setting3' => NULL,
-                                    'setting4' => 'blah blah',
-                                    'setting5' =>
+                                    'name' => 'follow',
+                                    'type' => 'added',
+                                    'value' => false,
+                                    'multilevel' => false,
+                                ],
+                                [
+                                    'name' => 'setting4',
+                                    'type' => 'added',
+                                    'value' => 'blah blah',
+                                    'multilevel' => false,
+                                ],
+                                [
+                                    'name' => 'setting5',
+                                    'type' => 'added',
+                                    'value' =>
                                         [
-                                            'key5' => 'value5',
-                                        ],
-                                    'setting6' =>
-                                        [
-                                            'key' => 'value',
-                                            'ops' => 'vops',
-                                            'doge' =>
+                                            0 =>
                                                 [
-                                                    'wow' => 'so much',
+                                                    'name' => 'key5',
+                                                    'type' => 'no_change',
+                                                    'value' => 'value5',
+                                                ],
+                                        ],
+                                    'multilevel' => true,
+                                ],
+                        ],
+                ],
+                [
+                    'name' => 'group1',
+                    'type' => 'changed',
+                    'multilevel' => true,
+                    'value' =>
+                        [
+                                [
+                                    'name' => 'baz',
+                                    'type' => 'changed',
+                                    'value_added' => 'bars',
+                                    'value_deleted' => 'bas',
+                                ],
+                                [
+                                    'name' => 'foo',
+                                    'type' => 'no_change',
+                                    'value' => 'bar',
+                                ],
+                                [
+                                    'name' => 'nest',
+                                    'type' => 'changed',
+                                    'value_added' => 'str',
+                                    'value_deleted' =>
+                                        [
+                                                [
+                                                    'name' => 'key',
+                                                    'type' => 'no_change',
+                                                    'value' => 'value',
                                                 ],
                                         ],
                                 ],
                         ],
+                ],
+                [
+                    'name' => 'group2',
+                    'type' => 'deleted',
+                    'value' =>
                         [
-                            'name' => 'group1',
-                            'type' => 'added',
-                            'value' =>
                                 [
-                                    'foo' => 'bar',
-                                    'baz' => 'bars',
-                                    'nest' => 'str',
+                                    'name' => 'abc',
+                                    'type' => 'no_change',
+                                    'value' => 12345,
                                 ],
-                        ],
-                        [
-                            'name' => 'group3',
-                            'type' => 'added',
-                            'value' =>
                                 [
-                                    'deep' =>
+                                    'name' => 'deep',
+                                    'type' => 'no_change',
+                                    'multilevel' => true,
+                                    'value' =>
                                         [
-                                            'id' =>
                                                 [
-                                                    'number' => 45,
-                                                ],
-                                        ],
-                                    'fee' => 100500,
-                                ],
-                        ],
-                    ],
-            ],
-            [
-                'name' => 'group1',
-                'type' => 'changed',
-                'multilevel' => true,
-                'value' =>
-                    [
-                        [
-                            'name' => 'baz',
-                            'type' => 'deleted',
-                            'value' => 'bas',
-                        ],
-                        [
-                            'name' => 'foo',
-                            'type' => 'deleted',
-                            'value' => 'bar',
-                        ],
-                        [
-                            'name' => 'nest',
-                            'type' => 'deleted',
-                            'value' =>
-                                [
-                                    'key' => 'value',
-                                ],
-                        ],
-                        [
-                            'name' => 'common',
-                            'type' => 'added',
-                            'value' =>
-                                [
-                                    'follow' => false,
-                                    'setting1' => 'Value 1',
-                                    'setting3' => NULL,
-                                    'setting4' => 'blah blah',
-                                    'setting5' =>
-                                        [
-                                            'key5' => 'value5',
-                                        ],
-                                    'setting6' =>
-                                        [
-                                            'key' => 'value',
-                                            'ops' => 'vops',
-                                            'doge' =>
-                                                [
-                                                    'wow' => 'so much',
+                                                    'name' => 'id',
+                                                    'type' => 'no_change',
+                                                    'value' => 45,
                                                 ],
                                         ],
                                 ],
                         ],
+                    'multilevel' => true,
+                ],
+                [
+                    'name' => 'group3',
+                    'type' => 'added',
+                    'value' =>
                         [
-                            'name' => 'group1',
-                            'type' => 'added',
-                            'value' =>
                                 [
-                                    'foo' => 'bar',
-                                    'baz' => 'bars',
-                                    'nest' => 'str',
-                                ],
-                        ],
-                        [
-                            'name' => 'group3',
-                            'type' => 'added',
-                            'value' =>
-                                [
-                                    'deep' =>
+                                    'name' => 'deep',
+                                    'type' => 'no_change',
+                                    'multilevel' => true,
+                                    'value' =>
                                         [
-                                            'id' =>
                                                 [
-                                                    'number' => 45,
+                                                    'name' => 'id',
+                                                    'type' => 'no_change',
+                                                    'multilevel' => true,
+                                                    'value' =>
+                                                        [
+                                                                [
+                                                                    'name' => 'number',
+                                                                    'type' => 'no_change',
+                                                                    'value' => 45,
+                                                                ],
+                                                        ],
                                                 ],
                                         ],
-                                    'fee' => 100500,
+                                ],
+                                [
+                                    'name' => 'fee',
+                                    'type' => 'no_change',
+                                    'value' => 100500,
                                 ],
                         ],
-                    ],
-            ],
-
-            [
-                'name' => 'group2',
-                'type' => 'deleted',
-                'value' =>
-                    [
-                        'abc' => 12345,
-                        'deep' =>
-                            [
-                                'id' => 45,
-                            ],
-                    ],
-            ],
-
-            [
-                'name' => 'group3',
-                'type' => 'added',
-                'value' =>
-                    [
-                        'deep' =>
-                            [
-                                'id' =>
-                                    [
-                                        'number' => 45,
-                                    ],
-                            ],
-                        'fee' => 100500,
-                    ],
-            ],
+                    'multilevel' => true,
+                ],
         ];
 
         $tree = createTree($this->dataFirstFileMultilevel, $this->dataLastFileMultilevel);
