f3f95f609a12652efa10ccb0b0bdb128723279f0
Evgeny Goldin <goldin5@yandex.ru>
2021-10-16 00:27:20 +0400

    small fixes formatter default

--- src/Format.php
+++ src/Format.php
@@ -31,39 +31,73 @@ function defaultFormat(array $tree, int $step = 1): string
     $multiplicator = $step === 1 ? 0 : 2;
     $spaces = "\r" . PHP_EOL . str_repeat(" ", $multiplicator * $step);
     $formattedTree = array_map(function ($treeElement) use ($step, $spaces) {
-        $multiplicator = $step === 1 ? 4 : 2;
         if ($treeElement['multilevel'] === true) {
             if ($treeElement['multivalued'] === true) {
-                $strAdded = is_array($treeElement['value_added']) ? defaultFormat($treeElement['value_added'], $step + 1) : convertToString($treeElement['value_added']);
-                $strDeleted = is_array($treeElement['value_deleted']) ? defaultFormat($treeElement['value_deleted'], $step + 1) : convertToString($treeElement['value_deleted']);
-                return str_repeat(" ", ($multiplicator * $step) - 2) . "- {$treeElement['name']}: " . $strDeleted . $spaces
-                    . str_repeat(" ", ($multiplicator * $step) - 2) . "+ {$treeElement['name']}: " . $strAdded;
+                $strAdded = is_array($treeElement['value_added'])
+                    ? defaultFormat($treeElement['value_added'], $step + 1)
+                    : convertToString($treeElement['value_added']);
+                $strDeleted = is_array($treeElement['value_deleted'])
+                    ? defaultFormat($treeElement['value_deleted'], $step + 1)
+                    : convertToString($treeElement['value_deleted']);
+                return createString($treeElement['name'], $strDeleted, $step + 1, '-') . $spaces
+                    . createString($treeElement['name'], $strAdded, $step + 1, '+');
             } else {
-                if ($treeElement['type'] === 'deleted') {
-                    return str_repeat(" ", ($multiplicator * $step) - 2) . "- {$treeElement['name']}: " . defaultFormat($treeElement['value'], $step + 1);
-                } elseif ($treeElement['type'] === 'added') {
-                    return str_repeat(" ", ($multiplicator * $step) - 2) . "+ {$treeElement['name']}: " . defaultFormat($treeElement['value'], $step + 1);
-                } else {
-                    return str_repeat(" ", $multiplicator * $step) . "{$treeElement['name']}: " . defaultFormat($treeElement['value'], $step + 1);
-                }
+                $char = createChar($treeElement['type']);
+                return createString(
+                    $treeElement['name'],
+                    defaultFormat($treeElement['value'], $step + 1),
+                    $step,
+                    $char
+                );
             }
         } else {
             switch ($treeElement['type']) {
                 case 'changed':
-                    return str_repeat(" ", ($multiplicator * $step) - 2) . "- {$treeElement['name']}: " . convertToString($treeElement['value_deleted']) . $spaces
-                        . str_repeat(" ", ($multiplicator * $step) - 2) . "+ {$treeElement['name']}: " . convertToString($treeElement['value_added']);
-                case 'deleted':
-                    return str_repeat(" ", ($multiplicator * $step) - 2) . "- {$treeElement['name']}: " . convertToString($treeElement['value']);
-                case 'added':
-                    return str_repeat(" ", ($multiplicator * $step) - 2) . "+ {$treeElement['name']}: " . convertToString($treeElement['value']);
+                    return createString(
+                            $treeElement['name'],
+                            convertToString($treeElement['value_deleted']),
+                            $step,
+                            '-'
+                        ) . $spaces .
+                        createString($treeElement['name'], convertToString($treeElement['value_added']), $step, '+');
+                case 'deleted' || 'added':
+                    $char = createChar($treeElement['type']);
+                    return createString(
+                        $treeElement['name'],
+                        convertToString($treeElement['value']),
+                        $step,
+                        $char
+                    );
             }
         }
     }, $tree);
-    $clearDataDefault = array_filter($formattedTree);
-    $resize = array_map(function ($elementTree) {
+    $clearData = resize($formattedTree);
+    return '{' . $spaces . implode($spaces, $clearData) . $spaces . '}';
+}
+
+function resize(array $dataDefault): array
+{
+    $clearData = array_filter($dataDefault);
+    return array_map(function ($elementTree) {
         return substr($elementTree, -1) === PHP_EOL ? substr($elementTree, 0, strlen($elementTree) - 1) : $elementTree;
-    }, $clearDataDefault);
-    return '{' . $spaces . implode($spaces, $resize) . $spaces . '}';
+    }, $clearData);
+}
+
+function createString(string $name, string $value, int $step, string $char = ''): string
+{
+    $multiplicator = $step === 1 ? 4 : 2;
+    $name = $char === '' ? "$name: " : "$char $name: ";
+    $repeat = $char === '' ? $multiplicator * $step : ($multiplicator * $step) - 2;
+    return str_repeat(" ", $repeat) . $name . $value;
+}
+
+function createChar(string $type): string
+{
+    return match ($type) {
+        'deleted' => '-',
+        'added' => '+',
+        default => ''
+    };
 }
 
 function jsonFormat(array $tree, int $step = 1): string
@@ -101,11 +135,8 @@ function jsonFormat(array $tree, int $step = 1): string
             }
         }
     }, $tree);
-    $clearDataJson = array_filter($formattedTree);
-    $resize = array_map(function ($elementTree) {
-        return substr($elementTree, -1) === PHP_EOL ? substr($elementTree, 0, strlen($elementTree) - 1) : $elementTree;
-    }, $clearDataJson);
-    return '{' . $spaces . implode($spaces, $resize) . $spaces . '}';
+    $clearData = resize($formattedTree);
+    return '{' . $spaces . implode($spaces, $clearData) . $spaces . '}';
 }
 
 function plainFormat(array $tree, int $step = 1, array $structureName = []): string
@@ -144,11 +175,8 @@ function plainFormat(array $tree, int $step = 1, array $structureName = []): str
             }
         }
     }, $tree);
-    $clearDataPlain = array_filter($formattedTree);
-    $resize = array_map(function ($elementTree) {
-        return substr($elementTree, -1) === PHP_EOL ? substr($elementTree, 0, strlen($elementTree) - 1) : $elementTree;
-    }, $clearDataPlain);
-    return implode(PHP_EOL, $resize);
+    $clearData = resize($formattedTree);
+    return implode(PHP_EOL, $clearData);
 }
 
 function getPlainStatus(string $typeElement): string
