2804d5aaa2dfedd80f12d21ebcec69f5c28594d9
Evgeny Goldin <goldin5@yandex.ru>
2021-10-26 23:20:30 +0400

    fix differ

---
+++ tests/ParsersTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use function Differ\Parsers\getFileData;
+
+
+class ParsersTest extends TestCase
+{
+    public $testData = array(
+        'host' => 'hexlet.io',
+        'timeout' => 50,
+        'proxy' => '123.234.53.22',
+        'follow' => false,
+    );
+
+    public function testYaml()
+    {
+        $fileData = getFileData('../tests/fixures/file1.yml');
+        $this->assertEquals($this->testData, $fileData);
+    }
+
+    public function testJson()
+    {
+        $fileData = getFileData('../tests/fixures/file3.json');
+        $this->assertEquals($this->testData, $fileData);
+    }
+}
