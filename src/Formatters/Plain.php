c6d0f106ddb45c9d801ab966d7845e05bff909a9
e.goldin <e.goldin@qsoft.ru>
2022-04-24 19:04:23 +0400

    fix filepath

--- src/Formatters/Plain.php
+++ src/Formatters/Plain.php
@@ -52,12 +52,7 @@ function getPlainStatus(string $typeElement): string
         default => ''
     };
 }
-function createStringResult(mixed $value): string
-{
-    return !is_null($value) && !is_bool($value) && !is_int($value)
-        ? "'" . convertToString($value) . "'"
-        : convertToString($value);
-}
+
 function convertToString(mixed $value): string
 {
     return match (true) {
